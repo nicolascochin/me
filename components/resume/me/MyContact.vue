18daf71d2f446da50d8fd19a894bfc9c837072dc
Nicolas COCHIN <account+cloud+github@coch.in>
2020-04-07 23:27:53 +0200

    remove item group

--- components/resume/me/MyContact.vue
+++ components/resume/me/MyContact.vue
@@ -1,22 +1,22 @@
 <template>
   <v-list dense flat>
-    <v-list-item-group >
-      <v-list-item
-        v-for="(item, i) in items"
-        :key="i"
-        :ripple="false"
-        link
-        :href="item.value"
-        target="_blank"
-      >
-        <v-list-item-icon>
-          <v-icon>{{item.icon}}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.value"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-item-group>
+    <v-list-item
+      v-for="(item, i) in items"
+      :key="i"
+      :ripple="false"
+      link
+      :href="item.value"
+      target="_blank"
+      :selectable="false"
+      dense
+    >
+      <v-list-item-icon>
+        <v-icon>{{item.icon}}</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.value"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
   </v-list>
 </template>
 
--- components/resume/me/MyPersonal.vue
+++ components/resume/me/MyPersonal.vue
@@ -1,20 +1,18 @@
 <template>
   <v-list dense flat>
-    <v-list-item-group color="primary">
-      <v-list-item
-        v-for="(item, i) in items"
-        :key="i"
-        :ripple="false"
-        :selectable="true"
-      >
-        <v-list-item-icon>
-          <v-icon>{{item.icon}}</v-icon>
-        </v-list-item-icon>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.value"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list-item-group>
+    <v-list-item
+      v-for="(item, i) in items"
+      :key="i"
+      :ripple="false"
+      :selectable="true"
+    >
+      <v-list-item-icon>
+        <v-icon>{{item.icon}}</v-icon>
+      </v-list-item-icon>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.value"></v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
   </v-list>
 </template>
 
