2797e0efe465ec73e86e26d2786af35d827270f9
Nicolas COCHIN <account+cloud+github@coch.in>
2020-04-08 11:50:30 +0200

    test chunk size

--- nuxt.config.js
+++ nuxt.config.js
@@ -141,9 +141,9 @@ export default {
     ** You can extend webpack config here
     */
     extend (config, ctx) {
-      if (ctx && ctx.isClient) {
-        config.optimization.splitChunks.maxSize = 51200
-      }
+      // if (ctx && ctx.isClient) {
+      //   config.optimization.splitChunks.maxSize = 51200
+      // }
       // if (ctx && ctx.isClient) {
       //   config.optimization.splitChunks.maxSize = 51200 * 18
       // }
